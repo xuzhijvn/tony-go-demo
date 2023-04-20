8a1759c66c232faf2dd33741549c87899ba76616
xuzhijvn <xuzhijun256>
2023-04-20 21:37:34 +0800

    test go init func

---
+++ init/conf/conf.go
@@ -0,0 +1,9 @@
+package conf
+
+import "fmt"
+
+func init() {
+	fmt.Println("hello init")
+}
+
+var Name = "dd"
---
+++ init/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"tony-go-demo/init/conf"
+)
+
+func main() {
+
+	fmt.Println(conf.Name)
+
+	fmt.Println(conf.Name)
+
+}
